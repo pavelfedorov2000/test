eff9ec586aa081465494549109b2fbb5d3836a3a
Pavel <pavelfedorov929@gmail.com>
2020-12-04 22:04:47 +0300

    докоммитил

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,9 +23,19 @@ $(function(){
             data: $(this).serialize() // Данные, которые отправляются
         }).done(function() {
             $(this).find("input").val(""); // Очищаем инпут
-            $('.overlay, #thanks').fadeIn('slow');
+            $('.overlay, .modal').fadeIn('slow');
             $('form').trigger('reset'); //Очистка формы
         });
         return false;
+    });
+
+    // Маска ввода
+    $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+    $('.form__btn').on('click', function() {
+        $('.overlay, .modal').fadeIn('slow');
+    }); // для проверки модального окна без сервера
+    $('.modal__close').on('click', function() {
+        $('.overlay, .modal').fadeOut('slow');
     });
 });
